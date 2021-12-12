4a56a85b2edd202d06fcb386cd837ebc6426ab33
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2021-12-12 04:19:12 +0100

    changes

--- stacks/build/gateway/files/build-app.sh
+++ stacks/build/gateway/files/build-app.sh
@@ -2,7 +2,8 @@ PHARMACY_API_URL=$1
 
 cd app || exit
 export API_HOST=${PHARMACY_API_URL}
-envsubst < environment.ts.template > ./src/environments/environment.ts || exit
+envsubst < environment.ts.template > ./frontend/src/environments/environment.ts || exit
+cd frontend || exit
 npm run build --prod && \
 cd dist && \
-mv "$(find . -maxdepth 1 -type d | tail -n 1)" /app
+mv "$(find . -maxdepth 1 -type d | tail -n 1)" ./app     
